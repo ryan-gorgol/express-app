5d23ba59add5b073c0a664473867f831632937f9
Ryan Gorgol <ryangorgol@gmail.com>
2022-06-07 17:35:00 -0700

    attempting to connect to mongodb, not working at this commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 // docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 
 const app = express();
 
+mongoose.connect("mongodb://mongoadmin:Password1@192.168.80.2:27017/?authSource=admin")
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e))
+
 app.get("/", (req, res) => {
-  res.send("<h2>Hi there, sexy</h2>")
+  res.send("<h2>Hi there!</h2>")
 });
 
 const port = process.env.PORT || 3000
