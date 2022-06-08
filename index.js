9284970cde7d2fe2742bfdc13445946c454190f0
Ryan Gorgol <ryangorgol@gmail.com>
2022-06-07 18:12:24 -0700

    created config files to store environment variables

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
 
 // docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 
 const app = express();
 
-mongoose.connect("mongodb://mongoadmin:Password1@mongo:27017/?authSource=admin")
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
   .then(() => console.log("successfully connected to DB"))
   .catch((e) => console.log(e))
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hi there</h2>")
+  res.send("<h2>Hi there!</h2>")
 });
 
 const port = process.env.PORT || 3000
