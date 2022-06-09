52f0d95b6e3377ed2322cfd3edf90da8b00d65be
Ryan Gorgol <ryangorgol@gmail.com>
2022-06-09 08:49:39 -0700

    implemented login route to enable signup users to login. no session storage

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,9 @@ const bcrypt = require("bcryptjs")
 
 exports.signUp = async (req, res) => {
   const { username, password } = req.body
-  const hashpassword = await bcrypt.hash(password, 12)
+  
   try {
+    const hashpassword = await bcrypt.hash(password, 12)
     const newUser = await User.create({
       username,
       password: hashpassword
@@ -16,6 +17,38 @@ exports.signUp = async (req, res) => {
         user: newUser
       }
     });
+  } catch (e) {
+    res.status(400).json({
+      status: "Failed"
+    })
+  }
+}
+
+exports.login = async (req, res) => {
+  const {username, password} = req.body
+  
+  try {
+    const user = await User.findOneAndDelete({ username })
+    
+    if (!user) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'user not found'
+      })
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password)
+
+    if (isCorrect) {
+      res.status(200).json({
+        status: 'success'
+      })
+    } else {
+      res.status(400).json({
+        status: 'failed',
+        message: 'incorrect username or password'
+      })
+    }
   } catch (e) {
     res.status(400).json({
       status: "Failed"
