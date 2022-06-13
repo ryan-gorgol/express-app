0adb7fb90e8dca0a0afa7eefdc0b6e6e621d4204
Ryan Gorgol <ryangorgol@gmail.com>
2022-06-13 09:32:03 -0700

    attempting to fix CORS issue

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ connectWithRetry();
 // json middleware
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", ""); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // APP ROUTES
 app.get("/", (req, res, next) => {
   res.send("<h2>Hi there!!</h2>")
