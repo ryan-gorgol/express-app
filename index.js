1c7ef32133093723a15d525647716fb0760e4eba
Ryan Gorgol <ryangorgol@gmail.com>
2022-06-13 11:47:20 -0700

    huh... trying a different method for changing headers

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // json middleware
 app.use(express.json());
 
