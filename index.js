647d003e87bf91142c6d42cc735869c51070cb66
Ryan Gorgol <ryangorgol@gmail.com>
2022-06-13 09:35:34 -0700

    take 3

--- index.js
+++ index.js
@@ -50,14 +50,13 @@ connectWithRetry();
 //   }
 // }))
 
+var cors = require('cors');
+app.use(cors());
+
 // json middleware
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "50.116.10.181"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 // APP ROUTES
 app.get("/", (req, res, next) => {
