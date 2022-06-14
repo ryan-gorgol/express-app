b20892124b76eafac100d25070ce263634fbc020
Ryan Gorgol <ryangorgol@gmail.com>
2022-06-14 10:56:25 -0700

    trying to manually create res headers

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const corsOptions = {
 }
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost:3000/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
