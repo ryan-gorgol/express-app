98513854913c83e7476ba576fe14e6ccb554a0ac
Ryan Gorgol <ryangorgol@gmail.com>
2022-07-19 11:09:32 -0700

    refactored .env file to be utilized during docker-compose

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
